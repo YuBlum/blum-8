4efc60df45983a9f26c22d60c0eae54cce0fa74d
blum <yuuybonito@gmail.com>
2023-05-13 20:12:37 -0300

    planning

---
+++ dev/include/bus.h
@@ -0,0 +1,16 @@
+#ifndef __BUS_H__
+#define __BUS_H__
+
+enum {
+	MEM_CPU,
+	MEM_RPU,
+	MEM_CAR /*cartridge memory*/
+};
+
+/*65536*/
+
+#define MEM_CPU_CAP 65536
+#define MEM_RPU_CAP 65536
+#define MEM_CAR_CAP
+
+#endif/*__BUS_H__*/
---
+++ dev/include/cpu.h
@@ -0,0 +1,6 @@
+#ifndef __CPU_H__
+#define __CPU_H__
+
+#include <types.h>
+
+#endif/*__CPU_H__*/
